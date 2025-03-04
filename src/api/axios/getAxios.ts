27aaa9e8b67cafe24d13a05d9048b6b2d419fbec
Вадим <pvs.versia@gmail.com>
2025-03-04 15:01:00 +0200

    CORS

--- src/api/axios/getAxios.ts
+++ src/api/axios/getAxios.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
 
 export const getAxios = (baseUrl: string) =>
-  axios.create({
-    baseURL: baseUrl,
-    timeout: 1000,
-    headers: {
-      accept: "application/json",
-    },
-      withCredentials:true,
+    axios.create({
+        baseURL: baseUrl,
+        timeout: 1000,
+        headers: {
+            accept: "application/json",
+            'Access-Control-Allow-Origin': 'http://localhost:3000',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        },
+    });
 
-  });
+
